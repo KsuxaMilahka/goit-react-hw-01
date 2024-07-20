30dbe9621e2bb19d31cfe0ee0cb51a917516ca37
KsuxaMilahka <ksuxamilahka35@gmail.com>
2024-07-20 23:17:26 +0200

    task3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Profile from './components/Profile/Profile.jsx';
 import userData from './components/Profile/userData.json';
+
 import friends from './components/Friends/friends.json';
 import FriendList from './components/Friends/FriendList.jsx';
 import styles from './components/Friends/FriendList.module.css';
+
+import TransactionHistory from './components/TransactionHistory/TransactionHistory.jsx';
+import transactions from './components/TransactionHistory/transactions.json';
 
 const App = () => {
   return (
@@ -18,6 +22,8 @@ const App = () => {
       <div className={styles.friendList}>
         <FriendList friends={friends} />
       </div>
+
+      <TransactionHistory items={transactions} />
     </>
   );
 };
